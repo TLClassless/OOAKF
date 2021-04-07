3d0a2b09b7e6fbee7b2e3cda8ee6bc7c58783bd1
Ben <classle55sc2@gmail.com>
2021-04-07 13:57:15 +0100

    added clips discovery functionality, this is only available in the deprecated v5 API, so may need to switch out when new method becomes available

--- src/components/home/bottomContainer.js
+++ src/components/home/bottomContainer.js
@@ -3,15 +3,26 @@ import style from "../../styles/BottomContainer.module.css";
 import ReactDOM from "react-dom";
 import axios from "axios";
 
-axios.componentDidMount(){
-  console.log(CLIENT_ID)
-}
-
 const CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
-
 const TWITCH_SECRET = process.env.REACT_APP_API_SECRET;
 
 export class bottomContainer extends Component {
+  componentDidMount() {
+    console.log(CLIENT_ID, TWITCH_SECRET);
+    const headers = {
+      "Client-ID": CLIENT_ID,
+      Accept: "application/vnd.twitchtv.v5+json",
+    };
+    axios
+      .get(
+        "https://api.twitch.tv/kraken/clips/top?limit=10&channel=moonmoon_ow",
+        { headers: headers }
+      )
+      .then(function (response) {
+        console.log(response);
+      });
+  }
+
   render() {
     return (
       <main className={style.container}>
