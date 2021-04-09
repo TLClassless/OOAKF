0c74e6de37d3e6256c7eb816305341c9fd8791fd
Ben <classle55sc2@gmail.com>
2021-04-09 17:42:29 +0100

    separated out twitchClips api interaction into its own component

--- src/components/home/bottomContainer.js
+++ src/components/home/bottomContainer.js
@@ -1,33 +1,12 @@
 import React, { Component } from "react";
 import style from "../../styles/BottomContainer.module.css";
-import axios from "axios";
-
-const CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
-const TWITCH_SECRET = process.env.REACT_APP_API_SECRET;
+import Clips from "./twitchClips.js";
 
 export class bottomContainer extends Component {
-  componentDidMount() {
-    console.log(CLIENT_ID, TWITCH_SECRET);
-    const headers = {
-      "Client-ID": CLIENT_ID,
-      Accept: "application/vnd.twitchtv.v5+json",
-    };
-    axios
-      .get(
-        "https://api.twitch.tv/kraken/clips/top?limit=10&period=all&channel=OOAKF",
-        {
-          headers: headers,
-        }
-      )
-      .then(function (response) {
-        console.log(response);
-        console.log(response.data.clips[0].title);
-      });
-  }
-
   render() {
     return (
       <main className={style.container}>
+        <Clips />
         <div className={style.clips}>clips</div>
         <div className={style.youtube}>youtube</div>
         <div className={style.vods}>vods</div>
