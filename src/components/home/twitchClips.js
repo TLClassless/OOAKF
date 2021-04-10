ff9d8a574e3b4346fc46b24f2a9c6caae3a82873
Ben <classle55sc2@gmail.com>
2021-04-10 22:52:41 +0100

    removed separate component as it wasn't functional, changing some things around to troubleshoot in twitchClips.js

--- src/components/home/twitchClips.js
+++ src/components/home/twitchClips.js
@@ -1,16 +1,42 @@
 import React, { Component } from "react";
 import style from "../../styles/BottomContainer.module.css";
-import { getData } from "../functional/getData.js";
+import axios from "axios";
+
+const CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
+const TWITCH_SECRET = process.env.REACT_APP_API_SECRET;
 
 export class twitchClips extends Component {
-  render() {
-    getData();
+  componentDidMount() {
+    console.log(CLIENT_ID, TWITCH_SECRET);
 
+    const headers = {
+      "Client-ID": CLIENT_ID,
+      Accept: "application/vnd.twitchtv.v5+json",
+    };
+
+    axios
+      .get(
+        "https://api.twitch.tv/kraken/clips/top?limit=10&period=all&channel=OOAKF",
+        {
+          headers: headers,
+        }
+      )
+      .then((response) => {
+        const thisData = response.data.clips;
+        console.log(thisData);
+        console.log(thisData[0].title);
+        console.log(thisData[0].url);
+        console.log(thisData[0].thumbnails.small);
+      })
+      .catch((error) => console.error("Error"));
+  }
+
+  render() {
     return (
       <div>
         <div className={style.clipContainer}>
           <img
-            src={thisData.thumbnails.small}
+            src="https://clips-media-assets2.twitch.tv/41523515292-offset-4512-preview-260x147.jpg"
             alt="thumbnail"
             className={style.thumbnail}
           />
