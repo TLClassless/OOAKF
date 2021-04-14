7bfb6a800e07a643987a7e0f3181cf4b50de779e
Ben <classle55sc2@gmail.com>
2021-04-14 18:39:39 +0100

    added youtube api

--- src/components/home/youtubeVideos.js
+++ src/components/home/youtubeVideos.js
@@ -13,19 +13,54 @@ export class youtubeVideos extends Component {
 
   async componentDidMount() {
     console.log(YOUTUBE_KEY);
-    const response = await youtube.get("/search", {
+    const response = await youtube.get("/playlistItems", {
       params: {
-        q: "boobies",
+        playlistId: "PL1Iie15gC15gNHozS35CbOelNb5ZbhQvx",
       },
     });
-    console.log(response);
-    //     this.setState({
-    // 	    videos: response.data
-    //     })
+    await this.setState({
+      videos: response.data.items,
+    });
   }
 
   render() {
-    return <div></div>;
+    console.log(this.state.videos);
+
+    const Array = this.state.videos;
+
+    const Titles = Array.map(function (element) {
+      return element.snippet.title;
+    });
+
+    const Thumbnails = Array.map(function (element) {
+      return element.snippet.thumbnails.medium.url;
+    });
+
+    const URLs = Array.map(function (element) {
+      return element.snippet.resourceId.videoId;
+    });
+
+    const URL = "https://youtube.com/" + URLs[0];
+
+    return (
+      <div>
+        <h1>hello</h1>
+
+        <div className={style.clipContainer}>
+          <a href={URL[2]}>
+            <img
+              src={Thumbnails[2]}
+              alt="thumbnail"
+              className={style.thumbnail}
+            />
+          </a>
+          <a href={URL[2]}>
+            <h3 className={style.clipTitle}>{Titles[2]}</h3>
+            {/* <h4 className={style.clipViews}>Views: {Views[2]}</h4> */}
+          </a>
+        </div>
+      </div>
+    );
   }
 }
 
