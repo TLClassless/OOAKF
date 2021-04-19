2a9a6eb9f01d3a2f88c6f1c01b79d615b9bad860
Ben <classle55sc2@gmail.com>
2021-04-19 17:29:18 +0100

    all elements of the speedrun required are now their own arrays

--- src/components/home/speedRuns.js
+++ src/components/home/speedRuns.js
@@ -6,7 +6,11 @@ export class speedRuns extends Component {
   constructor() {
     super();
     this.state = {
-      runs: [],
+      gameName: [],
+      gameThumbnail: [],
+      runTime: [],
+      runPlace: [],
+      runLink: [],
     };
   }
 
@@ -15,19 +19,40 @@ export class speedRuns extends Component {
       .get("https://www.speedrun.com/api/v1/users/8grze17x/personal-bests")
       .then((response) => {
         const thisData = response.data.data;
+        let runTimes = [];
+        let runPlaces = [];
+        let runLinks = [];
+        let gameNames = [];
+        let gameThumbnails = [];
+
+        thisData.forEach((element) => {
+          runTimes.push(element.run.times.primary_t);
+          runPlaces.push(element.place);
+          runLinks.push(element.run.weblink);
+        });
         const gameNameUri = thisData.map(function (element) {
           return element.run.links[1].uri;
         });
-        console.log(gameNameUri);
-        axios.get(gameNameUri[0]).then((response) => {
-          console.log(response.data);
-          const gameName = response.data.data.names.international;
-          const gameThumbnail = response.data.data.assets["cover-large"].uri;
-          console.log(gameName, gameThumbnail);
+
+        gameNameUri.forEach((element) =>
+          axios.get(element).then((response) => {
+            console.log(response.data);
+            gameNames.push(response.data.data.names.international);
+            gameThumbnails.push(response.data.data.assets["cover-large"].uri);
+          })
+        );
+        console.log(runLinks);
+        this.setState({
+          gameName: gameNames,
+          gameThumbnail: gameThumbnails,
+          runTime: runTimes,
+          runPlace: runPlaces,
+          runLink: runLinks,
         });
       });
   }
   render() {
+    console.log(this.state.gameName);
     return <div></div>;
   }
 }
