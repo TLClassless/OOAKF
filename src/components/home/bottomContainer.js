c8aaa719c32f4332c7fc12ea94d69a8973c29626
Ben <classle55sc2@gmail.com>
2021-05-27 15:44:12 +0100

    commented out YouTube for the live branch, waiting to fix secrets issue

--- src/components/home/bottomContainer.js
+++ src/components/home/bottomContainer.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import style from "../../styles/BottomContainer.module.css";
 import Clips from "./twitchClips.js";
 import VODs from "./twitchVods.js";
-import Youtube from "./youtubeVideos.js";
+// import Youtube from "./youtubeVideos.js";
 // import SpeedRun from "./speedRuns.js";
 
 export class bottomContainer extends Component {
@@ -19,12 +19,12 @@ export class bottomContainer extends Component {
         <h1 className={style.titleLeftAligned}>Recent Twitch VODs</h1>
 
         <VODs />
-        <h1 className={style.titleLeftAligned}>
+        {/* <h1 className={style.titleLeftAligned}>
           <a href="https://www.youtube.com/playlist?list=PL1Iie15gC15gNHozS35CbOelNb5ZbhQvx">
             The Lie in Podcast
           </a>
         </h1>
-        <Youtube />
+        <Youtube /> */}
         {/* <h1 className={style.titleLeftAligned}>Personal Best Speed Runs</h1>
         <SpeedRun /> */}
       </main>
